74852d14ad665858340c77df1d1d4ee682bb8976
Naomi <elsteinnaomi@gmail.com>
2019-03-20 11:37:57 +0200

    no diff

---
+++ SC_Unimplemented/src/edu/cg/ImageProcessor.java
@@ -0,0 +1,121 @@
+package edu.cg;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class ImageProcessor extends FunctioalForEachLoops {
+
+	// MARK: fields
+	public final Logger logger;
+	public final BufferedImage workingImage;
+	public final RGBWeights rgbWeights;
+	public final int inWidth;
+	public final int inHeight;
+	public final int workingImageType;
+	public final int outWidth;
+	public final int outHeight;
+
+	// MARK: constructors
+	public ImageProcessor(Logger logger, BufferedImage workingImage, RGBWeights rgbWeights, int outWidth,
+			int outHeight) {
+		super(); // initializing for each loops...
+
+		this.logger = logger;
+		this.workingImage = workingImage;
+		this.rgbWeights = rgbWeights;
+		inWidth = workingImage.getWidth();
+		inHeight = workingImage.getHeight();
+		workingImageType = workingImage.getType();
+		this.outWidth = outWidth;
+		this.outHeight = outHeight;
+		setForEachInputParameters();
+	}
+
+	public ImageProcessor(Logger logger, BufferedImage workingImage, RGBWeights rgbWeights) {
+		this(logger, workingImage, rgbWeights, workingImage.getWidth(), workingImage.getHeight());
+	}
+
+	// MARK: change picture hue - example
+	public BufferedImage changeHue() {
+		logger.log("Preparing for hue changing...");
+
+		int r = rgbWeights.redWeight;
+		int g = rgbWeights.greenWeight;
+		int b = rgbWeights.blueWeight;
+		int max = rgbWeights.maxWeight;
+
+		BufferedImage ans = newEmptyInputSizedImage();
+
+		forEach((y, x) -> {
+			Color c = new Color(workingImage.getRGB(x, y));
+			int red = r * c.getRed() / max;
+			int green = g * c.getGreen() / max;
+			int blue = b * c.getBlue() / max;
+			Color color = new Color(red, green, blue);
+			ans.setRGB(x, y, color.getRGB());
+		});
+
+		logger.log("Changing hue done!");
+
+		return ans;
+	}
+
+	public final void setForEachInputParameters() {
+		setForEachParameters(inWidth, inHeight);
+	}
+
+	public final void setForEachOutputParameters() {
+		setForEachParameters(outWidth, outHeight);
+	}
+
+	public final BufferedImage newEmptyInputSizedImage() {
+		return newEmptyImage(inWidth, inHeight);
+	}
+
+	public final BufferedImage newEmptyOutputSizedImage() {
+		return newEmptyImage(outWidth, outHeight);
+	}
+
+	public final BufferedImage newEmptyImage(int width, int height) {
+		return new BufferedImage(width, height, workingImageType);
+	}
+
+	// A helper method that deep copies the current working image.
+	public final BufferedImage duplicateWorkingImage() {
+		BufferedImage output = newEmptyInputSizedImage();
+		setForEachInputParameters();
+		forEach((y, x) -> output.setRGB(x, y, workingImage.getRGB(x, y)));
+
+		return output;
+	}
+
+	/**
+	 * Takes a colored image and returns it in greyscale
+	 * @return greyscale BufferedImage
+	 */
+	public BufferedImage greyscale() {
+		logger.log("Preparing for greyscaling...");
+
+		BufferedImage greyscaleImage = newEmptyInputSizedImage();
+
+		// for each pixel, average the red, green and blue weights. set their weights to the average
+		forEach((y, x) -> {
+			//get the colors for pixel at x,y in image
+			Color coloredPixel = new Color(workingImage.getRGB(x, y));
+			int weightedColor = coloredPixel.getRed() * rgbWeights.redWeight +
+								coloredPixel.getGreen() * rgbWeights.greenWeight +
+								coloredPixel.getBlue() * rgbWeights.blueWeight;
+			weightedColor /= (rgbWeights.redWeight + rgbWeights.greenWeight + rgbWeights.blueWeight);
+
+			Color greyscalePixel = new Color(weightedColor, weightedColor, weightedColor);
+			greyscaleImage.setRGB(x, y, greyscalePixel.getRGB());
+
+		});
+		return greyscaleImage;
+	}
+
+	public BufferedImage nearestNeighbor() {
+		// TODO: Implement this method, remove the exception.
+		throw new UnimplementedMethodException("nearestNeighbor");
+	}
+}
